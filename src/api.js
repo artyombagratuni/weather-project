e330f6fb67376fa59ba86980e534f2359c93e47c
Artyom <artyombagratuni1@gmail.com>
2023-02-18 15:02:45 +0400

    first commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'af3a5c6388msh19108f962d7d745p102260jsn10c794c03322',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "8f442d0f44b9f3d48ee51f4c599dba3a";
